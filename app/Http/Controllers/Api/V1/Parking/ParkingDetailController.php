009eba7b0c01816d923e760edb3ffa1b70459c18
Oness <auditoree9@gmail.com>
2026-09-22 11:47:16 +0000

    cleanup: v

--- app/Http/Controllers/Api/V1/Parking/ParkingDetailController.php
+++ app/Http/Controllers/Api/V1/Parking/ParkingDetailController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers\Api\V1\Parking;
 use App\Actions\Parking\ResolveParkingIdentifier;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ParkingDetailResource;
-use Illuminate\Http\Request;
 
 final class ParkingDetailController extends Controller
 {
     public function __invoke(
-        Request $request,
         string $parking,
         ResolveParkingIdentifier $resolveParkingIdentifier,
     ): ParkingDetailResource {
